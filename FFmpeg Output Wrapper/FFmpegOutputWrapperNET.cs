2128346a078883245ad0e2b6bc0306f2a7c30a3b
Konjiki no Yami <oscila91@gmail.com>
2023-05-16 12:39:04 +0200

    Updated version with bug fixes

--- FFmpeg Output Wrapper/FFmpegOutputWrapperNET.cs
+++ FFmpeg Output Wrapper/FFmpegOutputWrapperNET.cs
@@ -1,90 +1,139 @@
-﻿using System;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
+using System;
 
-namespace FFmpeg_Output_Wrapper
+public class FFmpegOutputWrapperNET
 {
-    public class FFmpegOutputWrapperNET
+    public String Frames = String.Empty;
+    public String Q = String.Empty;
+    public String Fps = String.Empty;
+    public size Size;
+
+    public struct size
     {
-        public String Frames = String.Empty;
-        public String Q = String.Empty;
-        public String Fps = String.Empty;
-        public String Size = String.Empty;
-        public TimeSpan Time;
-        public String Bitrate = String.Empty;
-        public String Speed = String.Empty;
-        public String EXCEPTIONS = String.Empty;
+        public Double Bytes { get; set; }
+        public Double Kilobytes { get; set; }
+        public Double Megabytes { get; set; }
+        public Double Gigabytes { get; set; }
+    }
 
-        public FFmpegOutputWrapperNET(String OutputFFmpeg)
-        {
-            FrameLineWrapper(OutputFFmpeg);
-        }
+    public TimeSpan Time;
+    public String Bitrate = String.Empty;
+    public String Speed = String.Empty;
+    public String EXCEPTIONS = String.Empty;
+    public String Output = String.Empty;
 
-        public void FrameLineWrapper(String OutputFFmpeg)
+    public FFmpegOutputWrapperNET(String OutputFFmpeg)
+    {
+        FrameLineWrapper(OutputFFmpeg);
+    }
+
+    void GetSize(String size)
+    {
+        if (String.IsNullOrWhiteSpace(size) == false)
         {
-            if (String.IsNullOrWhiteSpace(OutputFFmpeg) == false)
+            var temp = size.ToLower().Trim();
+            if (temp.Contains("kb"))
             {
-                if (OutputFFmpeg.Trim().StartsWith("frame"))
+                temp = temp.Replace("kb", String.Empty);
+                Size = new size()
                 {
-                    String[] Info = RemoveSpaces(OutputFFmpeg).Split(',');
-                    if (Info.Count() > 0)
+                    Bytes = Convert.ToDouble(temp) * 1024.0,
+                    Kilobytes = Convert.ToDouble(temp),
+                    Megabytes = Math.Round(Convert.ToDouble(temp) / 1024.0, 2),
+                    Gigabytes = Math.Round(Convert.ToDouble(temp) / 1024.0 / 1024.0, 2)
+                };
+            }
+        }
+    }
+
+    public void FrameLineWrapper(String OutputFFmpeg)
+    {
+        if (String.IsNullOrWhiteSpace(OutputFFmpeg) == false)
+        {
+            if (OutputFFmpeg.Trim().ToLower().StartsWith("frame") || OutputFFmpeg.Trim().ToLower().StartsWith("fps") || OutputFFmpeg.Trim().ToLower().StartsWith("q") || OutputFFmpeg.Trim().ToLower().StartsWith("size") || OutputFFmpeg.Trim().ToLower().StartsWith("time") || OutputFFmpeg.Trim().ToLower().StartsWith("bitrate") || OutputFFmpeg.Trim().ToLower().StartsWith("speed"))
+            {
+                String[] Info = RemoveSpaces(OutputFFmpeg).Split(',');
+                foreach (String s in Info)
+                {
+                    try
                     {
-                        String ecc = String.Empty;
-                        try
+                        if (s.ToLower().Contains("frame"))
                         {
-                            Frames = RemoveUntilFirstSymbol(Info[0]);
-                            Fps = RemoveUntilFirstSymbol(Info[1]);
-                            Q = RemoveUntilFirstSymbol(Info[2]);
-                            Size = RemoveUntilFirstSymbol(Info[3]);
-                            TimeSpan t = TimeSpan.Parse(RemoveUntilFirstSymbol(Info[4]));
+                            Frames = RemoveUntilFirstSymbol(s);
+                        }
+                        if (s.ToLower().Contains("fps"))
+                        {
+                            Fps = RemoveUntilFirstSymbol(s);
+                        }
+                        if (s.ToLower().Contains("q"))
+                        {
+                            Q = RemoveUntilFirstSymbol(s);
+                        }
+                        if (s.ToLower().Contains("size"))
+                        {
+                            GetSize(RemoveUntilFirstSymbol(s));
+                        }
+                        if (s.ToLower().Contains("bitrate"))
+                        {
+                            Bitrate = RemoveUntilFirstSymbol(s);
+                        }
+                        if (s.ToLower().Contains("speed"))
+                        {
+                            Speed = RemoveUntilFirstSymbol(s);
+                        }
+                        if (s.ToLower().Contains("time"))
+                        {
+                            TimeSpan t = TimeSpan.Parse(RemoveUntilFirstSymbol(s));
                             Time = new TimeSpan(t.Hours, t.Minutes, t.Seconds);
-                            Bitrate = RemoveUntilFirstSymbol(Info[5]);
-                            Speed = RemoveUntilFirstSymbol(Info[6]);
-                        }
-                        catch (Exception ex)
-                        {
-                            EXCEPTIONS = ex.Message;
                         }
                     }
-                }
-            }
-        }
-
-        protected String RemoveUntilFirstSymbol(String s)
-        {
-            for (Int32 i = 0; i < s.Length; i++)
-            {
-                if (Char.IsLetter(s[i]) == false)
-                {
-                    return s.Remove(0, i + 1);
-                }
-            }
-            return s;
-        }
-
-        protected String RemoveSpaces(String OutputWithSpaces)
-        {
-            Int32 FirstSpace = 0;
-            Char tmp = new Char();
-            StringBuilder sb = new StringBuilder();
-            foreach (Char c in OutputWithSpaces)
-            {
-                tmp = c;
-                if (Char.IsWhiteSpace(c))
-                {
-                    FirstSpace++;
-                    if (FirstSpace == 1)
+                    catch (Exception ex)
                     {
-                        tmp = ',';
+                        EXCEPTIONS = ex.Message;
                     }
                 }
-                else
-                {
-                    FirstSpace = 0;
-                }
-                sb.Append(tmp);
             }
-            return sb.ToString().Replace(" ", "").Replace("=,", "=");
+            else
+            {
+                Output = OutputFFmpeg;
+            }
         }
+    }
+
+    protected String RemoveUntilFirstSymbol(String s)
+    {
+        for (Int32 i = 0; i < s.Length; i++)
+        {
+            if (Char.IsLetter(s[i]) == false)
+            {
+                return s.Remove(0, i + 1);
+            }
+        }
+        return s;
+    }
+
+    protected String RemoveSpaces(String OutputWithSpaces)
+    {
+        Int32 FirstSpace = 0;
+        Char tmp = new Char();
+        StringBuilder sb = new StringBuilder();
+        foreach (Char c in OutputWithSpaces)
+        {
+            tmp = c;
+            if (Char.IsWhiteSpace(c))
+            {
+                FirstSpace++;
+                if (FirstSpace == 1)
+                {
+                    tmp = ',';
+                }
+            }
+            else
+            {
+                FirstSpace = 0;
+            }
+            sb.Append(tmp);
+        }
+        return sb.ToString().Replace(" ", "").Replace("=,", "=");
     }
 }
